d50d804f4e90d6d22e2cb9eb48f5c0b12b0c049c
gfbritto <gfbrito@outlook.com>
2021-05-11 20:01:27 -0300

    Add project files.

---
+++ Controllers/RepositoryController.cs
@@ -0,0 +1,26 @@
+﻿using BotGitHubApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BotGitHubApi.Controllers
+{
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class RepositoryController : ControllerBase
+    {
+
+        private readonly ILogger<RepositoryController> _logger;
+
+        public RepositoryController(ILogger<RepositoryController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Repository.GetGitHubRepositories());
+        }
+    }
+}
---
+++ Models/repository.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace BotGitHubApi.Models
+{
+    public class Repository
+    {
+        public string full_name { get; set; }
+        public string node_id { get; set; }
+        //public string description { get; set; }
+
+        public static List<Repository> GetGitHubRepositories()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+                client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+                var response = client.GetAsync("https://api.github.com/orgs/takenet/repos?sort=created&per_page=5&direction=desc").Result;
+                string responseBody = response.Content.ReadAsStringAsync().Result;
+                var repositories = JsonConvert.DeserializeObject<List<Repository>>(responseBody);
+                return repositories;
+            }
+        }
+    }
+}
